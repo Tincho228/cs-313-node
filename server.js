ba06f18de6168a4185a5ce8e85cefb510cac01ff
Tincho228 <martin_quintero521@hotmail.com>
2021-03-05 13:55:26 -0300

    Post calculator

--- server.js
+++ server.js
@@ -15,14 +15,16 @@ app.get('/getRate', function(req, res) {
     const weight = req.query.weight; 
     const mail_type =req.query.mail_type; 
     const postage = calculateRate(mail_type, weight); 
-    res.render('pages/result', {postage:postage});
+    res.render('pages/result', {postage:postage, weight:weight, mail_type:mail_type});
     
 });
 app.listen(PORT);
 console.log('8080 is the magic port');
+
+
 function calculateRate (mail_type, weight){
     let retail_parcels = [
-        { weight: '1', zone1: "4"},
+        { weight: '1', zone1: "$4"},
         { weight: '2', zone1: "4"},
         { weight: '3', zone1: "4"},
         { weight: '4', zone1: "4"},
@@ -37,7 +39,7 @@ function calculateRate (mail_type, weight){
         { weight: '13', zone1: "6,25"}
     ];
     let letter_stamped = [
-        { weight: '1', price: "0,55"},
+        { weight: '1', price: "$0,55"},
         { weight: '2', price: "0,75"},
         { weight: '3', price: "0,95"},
         { weight: '3,5', price: "1,15"}
@@ -49,21 +51,21 @@ function calculateRate (mail_type, weight){
         { weight: '3,5', price: "1,11"}
     ];
     let letter_flats = [
-        { weight: 1, zone1: "1"},
-        { weight: '2', zone1: "1,2"},
-        { weight: '3', zone1: "1,4"},
-        { weight: '4', zone1: "1,6"},
-        { weight: '5', zone1: "1,8"},
-        { weight: '6', zone1: "2"},
-        { weight: '7', zone1: "2,2"},
-        { weight: '8', zone1: "2,4"},
-        { weight: '9', zone1: "2,6"},
-        { weight: '10', zone1: "2,8"},
-        { weight: '11', zone1: "3,0"},
-        { weight: '12', zone1: "3,2"},
-        { weight: '13', zone1: "3,4"}
+        { weight: '1', price: "1"},
+        { weight: '2', price: "1,2"},
+        { weight: '3', price: "1,4"},
+        { weight: '4', price: "1,6"},
+        { weight: '5', price: "1,8"},
+        { weight: '6', price: "2"},
+        { weight: '7', price: "2,2"},
+        { weight: '8', price: "2,4"},
+        { weight: '9', price: "2,6"},
+        { weight: '10', price: "2,8"},
+        { weight: '11', price: "3,0"},
+        { weight: '12', price: "3,2"},
+        { weight: '13', price: "3,4"}
     ];
-    let postage = 0;
+    let postage = "Not found. For weights over 3.5 ounces, see Large Envelopes prices.";
     switch(mail_type){
       case 'stamped':
             letter_stamped.forEach(function(letter){
@@ -92,7 +94,6 @@ function calculateRate (mail_type, weight){
                 postage = letter.zone1;
                 }
             });
-            postage = parseFloat(num1) / parseFloat(num2);
             break;
       default:
             postage = "The operation is not supported";
